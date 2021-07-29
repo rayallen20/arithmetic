f0d93b42f5b9ad80d1c01bafd7e0dc95b276cdb7
rayallen20 <452857163@qq.com>
2021-07-30 02:15:05 +0800

    双端队列习题完成 链表实现
    明天开始进入本部分的对应课程笔记整理

--- class1/code/practice/palChecker/palChecker.go
+++ class1/code/practice/palChecker/palChecker.go
@@ -1,11 +1,32 @@
 package palChecker
 
+import (
+	"code/dataStructure/deque"
+)
+
 func  PalChecker(str string) (isPal bool) {
 	strRunes := []rune(str)
 	for i := 0; i < len(strRunes) / 2; i++ {
 		head := strRunes[i]
 		tail := strRunes[len(strRunes) - 1 - i]
 		if head != tail {
+			return false
+		}
+	}
+	return true
+}
+
+func PalCheckerByDeque(str string) (isPal bool) {
+	strRunes := []rune(str)
+	var charDeque deque.Deque = deque.Deque{}
+	for i := 0; i < len(strRunes); i++ {
+		charDeque.AddRear(strRunes[i])
+	}
+
+	for charDeque.Size() > 1 {
+		headChar, _ := charDeque.RemoveFront()
+		tailChar, _ := charDeque.RemoveRear()
+		if headChar != tailChar {
 			return false
 		}
 	}
